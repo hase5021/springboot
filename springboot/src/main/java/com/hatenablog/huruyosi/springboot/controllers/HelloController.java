7cc4c6193fb9bc41ee015e3a0464506144f35a39
huruyosi <huruyosi@yahoo.co.jp>
2015-07-26 08:43:30 +0900

    step3 テンプレートエンジンに thymeleaf を採用

--- springboot/src/main/java/com/hatenablog/huruyosi/springboot/controllers/HelloController.java
+++ springboot/src/main/java/com/hatenablog/huruyosi/springboot/controllers/HelloController.java
@@ -1,8 +1,12 @@
 package com.hatenablog.huruyosi.springboot.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -11,5 +15,12 @@ public class HelloController {
 	@ResponseBody
 	public String hello() {
 		return "hello world";
+	}
+	
+	@RequestMapping("/thymeleaf")
+	public ModelAndView thymeleaf() {
+		Map<String, String> model = new HashMap<String, String>();
+		model.put("message", "hello world");
+		return new ModelAndView("HelloController/thymeleaf", model);
 	}
 }
