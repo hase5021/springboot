f885430ac57db879e58cf281c75bec85a6dedac1
huruyosi <huruyosi@yahoo.co.jp>
2015-07-26 00:25:49 +0900

    その１ webプロジェクトを新規に作成

---
+++ springboot/src/main/java/com/hatenablog/huruyosi/springboot/App.java
@@ -0,0 +1,17 @@
+package com.hatenablog.huruyosi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Hello world!
+ * 
+ */
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
