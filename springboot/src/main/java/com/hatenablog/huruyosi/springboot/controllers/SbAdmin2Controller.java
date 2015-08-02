6ad3f6f13ed944cf82e003a7cdd9e75735e6cceb
huruyosi <huruyosi@yahoo.co.jp>
2015-08-02 10:53:55 +0900

    stpe5 レイアウトを作成

---
+++ springboot/src/main/java/com/hatenablog/huruyosi/springboot/controllers/SbAdmin2Controller.java
@@ -0,0 +1,15 @@
+package com.hatenablog.huruyosi.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/sbadmin2/")
+public class SbAdmin2Controller {
+	
+	@RequestMapping("/blank")
+	public ModelAndView blank() {
+		return new ModelAndView("SbAdmin2Controller/blank");
+	}
+}
