0ea87ac3a51f6a699ad9addd74c817465bd324d0
huruyosi <huruyosi@yahoo.co.jp>
2015-08-02 12:19:16 +0900

    step5 sbadmin2/index を追加

--- springboot/src/main/java/com/hatenablog/huruyosi/springboot/controllers/SbAdmin2Controller.java
+++ springboot/src/main/java/com/hatenablog/huruyosi/springboot/controllers/SbAdmin2Controller.java
@@ -7,8 +7,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/sbadmin2/")
 public class SbAdmin2Controller {
-	
-	@RequestMapping("/blank")
+
+	@RequestMapping("/index.html")
+	public ModelAndView index() {
+		return new ModelAndView("SbAdmin2Controller/index");
+	}
+
+	@RequestMapping("/blank.html")
 	public ModelAndView blank() {
 		return new ModelAndView("SbAdmin2Controller/blank");
 	}
